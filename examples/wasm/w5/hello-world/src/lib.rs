f32aaee98e3203e4eea5c4ec70e30f97403a571f
katopz <katopz@gmail.com>
2022-11-10 15:53:14 +0700

    working test but wasm test failed

--- examples/wasm/w5/hello-world/src/lib.rs
+++ examples/wasm/w5/hello-world/src/lib.rs
@@ -1,10 +1,11 @@
 mod utils;
 
+use serde_json::Value;
 use utils::set_panic_hook;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_test::wasm_bindgen_test;
 
 // app
-use reqwest::Client;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -13,16 +14,36 @@ use reqwest::Client;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub async fn fetch() -> Result<JsValue, JsError> {
+pub async fn fetch_js_value() -> Result<JsValue, JsError> {
     set_panic_hook();
 
-    let client = &Client::new();
-    let url = "https://github.com/katopz";
+    let resp = fetch_value().await?;
 
-    let resp = client.get(url).send().await?;
-    let bytes = resp.bytes().await?;
+    println!("resp:{:#?}", resp);
 
-    assert!(!bytes.is_empty());
+    Ok(serde_wasm_bindgen::to_value(&resp)?)
+}
 
-    Ok(serde_wasm_bindgen::to_value(&bytes.len())?)
+pub async fn fetch_value() -> Result<Value, JsError> {
+    set_panic_hook();
+
+    let resp = reqwest::get("https://raw.githubusercontent.com/gist-rs/book/main/examples/wasm/w5/hello-world/src/hello.json")
+        .await?
+        .json::<serde_json::Value>()
+        .await?;
+
+    println!("resp:{:#?}", resp);
+
+    Ok(resp)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::fetch_value;
+
+    #[tokio::test]
+    async fn test_fetch_value() {
+        let res = fetch_value().await;
+        assert!(res.is_ok());
+    }
 }
--- examples/wasm/w5/hello-world/tests/web.rs
+++ examples/wasm/w5/hello-world/tests/web.rs
@@ -3,11 +3,13 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+use hello_world::fetch_js_value;
 use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn fetch() {
+    let res = fetch_js_value().await.unwrap();
+    assert!(res.is_ok());
 }
