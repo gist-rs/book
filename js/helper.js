3bd2d6bef94a2879d110eb8d3d31c4a807249592
katopz <katopz@gmail.com>
2022-11-22 09:05:23 +0700

    fix: same origin

--- js/helper.js
+++ js/helper.js
@@ -1,7 +1,12 @@
 window.onload = () => {
   // Open external link as new windows.
   ;[...document.getElementsByTagName('a')].map((e) => {
-    if (e.href.startsWith('https://')) e.target = '_blank'
+    // Only external link and not targeted yet will open new window
+    if (e.href.startsWith('https://') && !e.href.startsWith(`https://${window.location.origin}`)) {
+      e.target = e.target || '_blank'
+      // Chrome < 88 still need this even targeted _blank.
+      e.rel = 'noopener'
+    }
     return e
   })
 }
