b74cb354f2edb59dcb565283eb0618a524164db5
katopz <katopz@gmail.com>
2022-11-22 10:44:15 +0700

    fix: origin bug

--- js/helper.js
+++ js/helper.js
@@ -2,7 +2,7 @@ window.onload = () => {
   // Open external link as new windows.
   ;[...document.getElementsByTagName('a')].map((e) => {
     // Only external link and not targeted yet will open new window
-    if (e.href.startsWith('https://') && !e.href.startsWith(`https://${window.location.origin}`)) {
+    if (e.href.startsWith('https://') && !e.href.startsWith(window.location.origin)) {
       e.target = e.target || '_blank'
       // Chrome < 88 still need this even targeted _blank.
       e.rel = 'noopener'
