fa2e5b047a8e33b1899401a6968590e57661b197
katopz <katopz@gmail.com>
2022-12-06 15:35:42 +0700

    add support

---
+++ js/support.js
@@ -0,0 +1,3 @@
+const div_right = document.querySelector('#menu-bar > div.right-buttons')
+const sponsor_html = '<a href="https://patreon.com/gist_rs" title="Sponsor" aria-label="Sponsor" target="_blank" rel="noopener"><i id="sponsor-button" class="fa fa-heart fa-beat beat-fade"></i></a>'
+div_right.innerHTML = sponsor_html + div_right.innerHTML
