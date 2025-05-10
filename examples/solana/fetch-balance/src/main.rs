b23967ee8c833c9d681a0bd3b75eb02ec9e7dd89
katopz <katopz@gmail.com>
2025-05-11 00:11:29 +0900

    fix: currency_rs no needed life time 🥸

--- examples/solana/fetch-balance/src/main.rs
+++ examples/solana/fetch-balance/src/main.rs
@@ -2,47 +2,40 @@ use currency_rs::{Currency, CurrencyOpts};
 use rand::Rng;
 use serde::{Deserialize, Serialize};
 
-// We need this lifetime 👇.
-struct GetBalanceOptions<'a> {
-    rpc_url: &'static str,
-    id: u32,
-    // Because of this options 👇 required it.
-    currency_opts: Option<CurrencyOpts<'a>>,
+pub struct GetBalanceOptions {
+    pub rpc_url: &'static str,
+    pub id: u32,
+    pub currency_opts: Option<CurrencyOpts>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct RpcPayload {
-    jsonrpc: String,
-    id: u32,
-    method: String,
-    params: Vec<String>,
+pub struct RpcPayload {
+    pub jsonrpc: String,
+    pub id: u32,
+    pub method: String,
+    pub params: Vec<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct GetBalanceResponse {
-    jsonrpc: String,
-    id: u32,
-    result: GetBalanceResponseResult,
+pub struct GetBalanceResponse {
+    pub jsonrpc: String,
+    pub id: u32,
+    pub result: GetBalanceResponseResult,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct GetBalanceResponseResult {
-    value: u64,
+pub struct GetBalanceResponseResult {
+    pub value: u64,
 }
 
 #[derive(Debug)]
 #[allow(dead_code)]
-struct UiBalance {
-    lamports: f64,
-    ui_lamports: String,
+pub struct UiBalance {
+    pub lamports: f64,
+    pub ui_lamports: String,
 }
 
-// We need this 'a  👇
-async fn get_balance<'a>(
-    pubkey: String,
-    // But this can be elided 'a👇
-    options: GetBalanceOptions<'_>,
-) -> anyhow::Result<UiBalance> {
+async fn get_balance(pubkey: String, options: GetBalanceOptions) -> anyhow::Result<UiBalance> {
     let client = reqwest::Client::new();
     let json_payload = RpcPayload {
         jsonrpc: "2.0".to_owned(),
@@ -76,7 +69,7 @@ async fn main() {
         // Consider donate some SOL to this 👇 account to see some number show up 😆
         "gistmeAhMG7AcKSPCHis8JikGmKT9tRRyZpyMLNNULq".to_owned(),
         GetBalanceOptions {
-            rpc_url: "https://rpc.ankr.com/solana",
+            rpc_url: "https://api.mainnet-beta.solana.com",
             id: rand::thread_rng().gen_range(0u32..u32::MAX),
             currency_opts: Some(
                 CurrencyOpts::new()
